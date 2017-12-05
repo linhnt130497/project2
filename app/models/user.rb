8e04f3fa9d3408f4bfef7bfc6da7aa67f02efa54
Nguyen Linh <linhnt130497@gmail.com>
2017-12-06 06:21:36 +0700

    static-pages

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  before_save {email.downcase!}
+  validates :name, presence: true, length: { Settings.user.name_maximum }
+  validates :password, presence: true, length: { Settings.user.password_minimum }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: Settings.user.email_maximum },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: true,
+            uniqueness: { case_sensitive: false }
+  has_secure_password
+end
